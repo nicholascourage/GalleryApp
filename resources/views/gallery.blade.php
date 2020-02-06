2b9592b4f62534d36781a080a7736b03dfd8ce17
nicholascourage <48243052+nicholascourage@users.noreply.github.com>
2020-02-06 17:41:09 +0000

    53. Delete confirmation with bootstrap modal

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -16,10 +16,37 @@
                 <div class="item">
                     <img src="{{asset('storage/'.$album->name)}}" class="img-thumbnail" style="width:300px">
                 </div>
-                <form action="{{route('image.delete')}}" method="POST">@csrf
-                    <input type="hidden" name="id" value="{{$album->id}}">
-                    <button class="btn btn-danger" type="submit">Delete</button>
-                </form>
+
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal{{$album->id}}">
+                    Delete
+                </button>
+                
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal{{$album->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Delete</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                Do you want to delete? {{$album->id}} 
+                            </div>
+                            <div class="modal-footer">
+                                <form action="{{route('image.delete')}}" method="POST">@csrf
+                                    <input type="hidden" name="id" value="{{$album->id}}">
+                                    <button class="btn btn-danger" type="submit">Delete</button>
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         @endforeach    
 
